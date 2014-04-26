60c01cea6cc4e418e4ac2a65ddda5ae1ed0fea13
Michael Goth <iqe@gmx.net>
2014-04-27 01:44:32 +0200

    Add first real Feature: DigitalInput

---
+++ DigitalInput.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <DigitalInput.h>
+
+Feature* DigitalInput::create(FeatureSpec spec) {
+  if (spec.pinCount == 1 && spec.configSize == sizeof(DigitalInputConfig)) {
+    return new DigitalInput(spec.pins[0], (DigitalInputConfig*)spec.config);
+  }
+
+  return 0;
+}
+
+DigitalInput::DigitalInput(uint8_t pin, DigitalInputConfig *config) {
+  this->pin = pin;
+  flags = config->flags;
+  debounceMillis = config->debounceMillis;
+
+  value = LOW;
+  changed = false;
+  lastChangedMillis = 0;
+
+  if (isSet(DI_FLAG_INPUT_PULLUP)) {
+    pinMode(pin, INPUT_PULLUP);
+  } else {
+    pinMode(pin, INPUT);
+  }
+}
+
+void DigitalInput::update() {
+  uint32_t now = millis();
+
+  changed = false;
+
+  if (now - lastChangedMillis >= debounceMillis) {
+    int16_t newValue = digitalRead(pin);
+
+    if (isSet(DI_FLAG_INPUT_PULLUP)) {
+      newValue = newValue ^ 1; // reverse; with pullup, GND is logical HIGH
+    }
+
+    if (isSet(DI_FLAG_REVERSED)) {
+      newValue = newValue ^ 1; // reverse
+    }
+
+    if (newValue != value) {
+      lastChangedMillis = now;
+      value = newValue;
+      changed = true;
+    }
+  }
+}
+
+bool DigitalInput::isChanged() {
+  return changed;
+}
+
+int16_t DigitalInput::sendMessage(char *buf, int16_t bufsize) {
+  if (bufsize >= sizeof(uint8_t)) {
+    buf[0] = value == HIGH ? 'H' : 'L';
+    return sizeof(uint8_t);
+  }
+  return 0;
+}
+
+bool DigitalInput::isSet(uint8_t flag) {
+  return flags & flag;
+}
+
---
+++ DigitalInput.h
@@ -0,0 +1,38 @@
+#ifndef DigitalInput_h
+#define DigitalInput_h
+
+#include <Feature.h>
+
+#define DI_FLAG_INPUT_PULLUP 1
+#define DI_FLAG_REVERSED     2
+
+struct DigitalInputConfig {
+  uint8_t flags;
+  uint32_t debounceMillis;
+};
+
+class DigitalInput : Feature {
+public:
+  static Feature* create(FeatureSpec spec);
+
+  void update();
+  bool isChanged();
+
+  int16_t sendMessage(char *buf, int16_t bufsize);
+
+private:
+  uint8_t pin;
+  uint8_t flags;
+  uint32_t debounceMillis;
+
+  int16_t value;
+  bool changed;
+  uint32_t lastChangedMillis;
+
+  DigitalInput(uint8_t pin, DigitalInputConfig *config);
+
+  bool isSet(uint8_t flag);
+};
+
+#endif
+
