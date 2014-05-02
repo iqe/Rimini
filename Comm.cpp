e0e2c508c71e0c6ddc0820ad7df572ee5e06069f
Michael Goth <iqe@gmx.net>
2014-05-02 23:37:02 +0200

    Refactor Comm to make it actually work

--- Comm.cpp
+++ Comm.cpp
@@ -5,12 +5,50 @@ Comm::Comm(Repository *repo) {
 }
 
 void Comm::readMessage(unsigned char *buf, int16_t msgsize) {
-  if (msgsize >= 2) { // sizeof(int16_t)
-    int *featureId = (int*)buf;
-    Feature *feature = repo->getFeature(*featureId);
-    if (feature != 0) {
-      feature->readMessage(&buf[2], msgsize - 2);
+  int16_t featureId = readFeatureId(buf, msgsize);
+
+  Feature *feature = repo->getFeature(featureId);
+  if (feature != 0) {
+    feature->readMessage(buf + sizeof(featureId), msgsize - sizeof(featureId));
+  }
+}
+
+int16_t Comm::readFeatureId(unsigned char *buf, int16_t msgsize) {
+  if (msgsize >= sizeof(int16_t)) {
+    return *(int16_t*)buf;
+  }
+  return -1; // -1 because 0 is used for Config feature
+}
+
+int16_t Comm::writeMessageIfChanged(int16_t featureId, unsigned char *buf, int16_t bufsize) {
+  Feature *feature = repo->getFeature(featureId);
+
+  if (feature != 0) {
+    feature->update();
+    if (feature->isChanged()) {
+      return writeMessage(featureId, feature, buf, bufsize);
     }
   }
+  return 0;
+}
+
+int16_t Comm::writeMessage(int16_t featureId, Feature *feature, unsigned char *buf, int16_t bufsize) {
+  int msgsize = 0;
+
+  msgsize += writeFeatureId(featureId, buf, bufsize);
+  msgsize += feature->writeMessage(buf + msgsize, bufsize - msgsize);
+
+  return msgsize;
+}
+
+int16_t Comm::writeFeatureId(int16_t featureId, unsigned char *buf, int16_t bufsize) {
+  if (bufsize >= sizeof(featureId)) {
+    buf[0] = featureId >> 8;
+    buf[1] = featureId;
+
+    return sizeof(featureId);
+  }
+
+  return 0;
 }
 
--- Comm.h
+++ Comm.h
@@ -4,14 +4,21 @@
 #include <Feature.h>
 #include <Repository.h>
 
-class Comm : Feature {
+class Comm {
 public:
   Comm(Repository* repo);
 
   void readMessage(unsigned char *buf, int16_t msgsize);
 
+  int16_t writeMessageIfChanged(int16_t featureId, unsigned char *buf, int16_t bufsize);
+
 private:
   Repository *repo;
+
+  int16_t writeMessage(int16_t featureId, Feature *feature, unsigned char *buf, int16_t bufsize);
+
+  int16_t readFeatureId(unsigned char *buf, int16_t msgsize);
+  int16_t writeFeatureId(int16_t featureId, unsigned char *buf, int16_t bufsize);
 };
 
 #endif
